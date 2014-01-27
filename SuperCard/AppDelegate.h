e76fefda93e7de8a17a5b14f8cbef903b3564cc5
Denis C de Azevedo <denis.azevedo@gmail.com>
2014-01-27 00:59:23 -0200

    Initial Commit

---
+++ SuperCard/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  SuperCard
+//
+//  Created by Denis C de Azevedo on 27/01/14.
+//  Copyright (c) 2014 Denis C de Azevedo. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ SuperCard/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  SuperCard
+//
+//  Created by Denis C de Azevedo on 27/01/14.
+//  Copyright (c) 2014 Denis C de Azevedo. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
