4ee7645ab807bc2e226bc2ef9dd8cc3edba9fbb8
Denis C de Azevedo <denis.azevedo@gmail.com>
2014-01-27 02:52:25 -0200

    add card faced up with images

---
+++ SuperCard/PlayingCardView.h
@@ -0,0 +1,17 @@
+//
+//  PlayingCardView.h
+//  SuperCard
+//
+//  Created by Denis C de Azevedo on 27/01/14.
+//  Copyright (c) 2014 Denis C de Azevedo. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface PlayingCardView : UIView
+
+@property (nonatomic) NSUInteger rank;
+@property (nonatomic, strong) NSString *suit;
+@property (nonatomic) BOOL faceUp;
+
+@end
--- SuperCard/ViewController.m
+++ SuperCard/ViewController.m
@@ -7,9 +7,10 @@
 //
 
 #import "ViewController.h"
+#import "PlayingCardView.h"
 
 @interface ViewController ()
-
+@property (weak, nonatomic) IBOutlet PlayingCardView *playingCardView;
 @end
 
 @implementation ViewController
@@ -18,12 +19,9 @@
 {
     [super viewDidLoad];
 	// Do any additional setup after loading the view, typically from a nib.
-}
-
-- (void)didReceiveMemoryWarning
-{
-    [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
+    self.playingCardView.suit = @"♥";
+    self.playingCardView.rank = 13;
+    self.playingCardView.faceUp = YES;
 }
 
 @end
